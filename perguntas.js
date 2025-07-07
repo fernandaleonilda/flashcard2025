3a1a4c6a50db42275a936cbfd13a79bb23b0ef2a
Fernanda diva <fernanda.vendramim.correa@escola.pr.gov.br>
2025-07-07 16:47:27 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,21 @@
+criaCartao (
+   'Madagascar',
+    'Qual o nome dos 4 pernosagens principais?',
+    'Alex, Marty, Gloria e Melman',
+    )
+    criaCartao (
+    'Madagascar',
+    'Onde os pernosagens principais são jogados?',
+    'Navio'
+)
+criaCartao (
+    'Madagascar',
+    'Quantos anos Marty fez no primeiro filme?',
+    '10 anos'
+)
+
+criaCartao (
+    'Madagascar',
+   ' Em que ano foi lançao o primeiro filme?',
+   ' 2005'
+)
